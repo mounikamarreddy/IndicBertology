9643beb53891cc943eda1faba7ad7479ac0c2747
Akhilesh <aforakhilesh>
2022-12-31 00:38:29 +0530

    Updated WordContent

--- src/wordContent/vocab.py
+++ src/wordContent/vocab.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from re import search
 from sys import argv
 import os
-
+import pandas as pd
 
 def find_file_list(folder_path):
     """Find file list inside a folder."""
@@ -20,18 +20,15 @@ def extract_raw_sentences(file_path):
     return raw_sentences
 
 
-def getTotalCorpus():
-    parser = ArgumentParser(description="This is a program for extracting raw sentences from dependency annotated treebanks.")
-    parser.add_argument('-i', dest='inp')
-    args = parser.parse_args()
+def getTotalCorpus(inp):
     sentences = []
-    if not os.path.isdir(args.inp):
-        raw_sentences = extract_raw_sentences(args.inp)
+    if not os.path.isdir(inp):
+        raw_sentences = extract_raw_sentences(inp)
         for item in raw_sentences:
             sentences.append(item)
 
     else:
-        file_list = find_file_list(args.inp)
+        file_list = find_file_list(inp)
         for fl in file_list:
             raw_sentences = extract_raw_sentences(fl)
             for item in raw_sentences:
@@ -45,13 +42,22 @@ def write_lines_to_file(lines, file_path):
         file_write.write('\n'.join(lines))
 
 def main():
+
+    parser = ArgumentParser(description="This is a program for extracting raw sentences from dependency annotated treebanks.")
+    parser.add_argument('-i', dest='inp')
+    parser.add_argument('-o',dest='out')
+    args = parser.parse_args()
+
     wordContent = []
-    sentences = getTotalCorpus()
+    sent = []
+    sentences = getTotalCorpus(args.inp)
     vocabulary = {}
     data = ""
+
     for item in sentences:
         data += item
         data += " "
+    data = data.split(" ")
     vocab = []
     for sentence in sentences:
         for item in sentence.split(" "):
@@ -66,22 +72,27 @@ def main():
             f.write("\n")
         f.close()
     vocabulary = dict(sorted(vocabulary.items(), key=lambda item: item[1]))
+    # print(vocabulary)
+
 
     sampled_words = []
     for item in vocabulary.keys():
-        if vocabulary[item] > 100 and vocabulary[item] < 200:
+        if vocabulary[item] > 25 and vocabulary[item] < 100:
             sampled_words.append(item)
-    
-    print(len(sampled_words))
 
     for word in sampled_words:
         count  = 0
         for sentence in sentences:
-            if sentence.count(word) == 1 and count <= 2:
+            if sentence.count(word) == 1 and count <= 5:
                 count += 1
-                wordContent.append(word+"---------"+sentence)
+                wordContent.append(word)
+                sent.append(sentence)
+
     
-    write_lines_to_file(wordContent, "wordContent.txt")
+    # write_lines_to_file(wordContent, "wordContent.txt")
+    d = {"WordContent":wordContent,"sentences":sent}
+    df = pd.DataFrame(d)
+    df.to_csv(args.out,index=False)
 
 
 if __name__ == '__main__':
